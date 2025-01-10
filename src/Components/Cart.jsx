f4066490de4cb52de135c0a36e19c88deee01681
JayJay247in <cj193532@gmail.com>
2025-01-10 15:48:15 -0500

    new files

---
+++ src/Components/Cart.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeItemFromCart, increaseItemQuantity, decreaseItemQuantity } from './CartSlice';
+import SuperCoin from './SuperCoin';
+
