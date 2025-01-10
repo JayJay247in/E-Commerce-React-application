4ffe10d1e05c1cb77a0bacfd86e64c516f5a04a4
JayJay247in <cj193532@gmail.com>
2025-01-10 18:36:46 -0500

    new files added and files modification

--- src/Components/Cart.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { removeItemFromCart, increaseItemQuantity, decreaseItemQuantity } from './CartSlice';
-import SuperCoin from './SuperCoin';
-
--- src/Components/SuperCoin.jsx
+++ src/Components/SuperCoin.jsx
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux'
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 const SuperCoin = () => {
     const [superCoins, setSuperCoins] = useState(0);
     const cartItems = useSelector((state) => state.cart.cartItems);
 
-    const totalAmount = cartItems.reduce((accumulator, item) => {
-        return accumulator + item.price * item.quantity;
+    useEffect(() => {
+        console.log("Cart Items in SuperCoin:", cartItems);
+    }, [cartItems]);
+
+    const totalAmount = cartItems.reduce((total, item) => {
+        return total + (item.price || 0) * (item.quantity || 0);
     }, 0);
 
     useEffect(() => {
+        console.log("Total Amount in SuperCoin:", totalAmount);
         if (totalAmount >= 300) {
             setSuperCoins(30);
         } else if (totalAmount >= 200) {
@@ -23,7 +27,7 @@ const SuperCoin = () => {
     }, [totalAmount]);
 
     return (
-        <div>
+        <div style={{ textAlign: 'center' }}>
             You have earned: {superCoins} Super Coins!
         </div>
     );
